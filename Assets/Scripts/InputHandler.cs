e749cdbd853165a582e5cec2f644a78009f60555
brianjernigan <brobertjernigan@gmail.com>
2024-02-13 18:20:20 -0500

    UI Setup
    Added buttons and script for input handling

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _yearInput;
+    [SerializeField] private TMP_InputField _makeInput;
+}
